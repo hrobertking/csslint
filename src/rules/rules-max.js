e58fed325cda61ba5ecd423d3303dc9ad10fb416
Robert King <hrobertking@cathmhaol.com>
2017-04-06 10:35:16 -0700

    Adding ability to pass rules-max limit using cli

--- src/rules/rules-max.js
+++ src/rules/rules-max.js
@@ -6,21 +6,25 @@ CSSLint.addRule({
 
     // rule information
     id: "rules-max",
-    name: "Warn when 500+ rules are present",
-    desc: "Will warn when the rule count in a stylesheet crosses the 500 threshold.",
-    browsers: "IE",
+    name: "Warn when too many rules are present",
+    desc: "Will warn when the rule count in a stylesheet crosses the limit specified.",
+    browsers: "All",
 
     // initialization
-    init: function(parser, reporter) {
+    init: function(parser, reporter, options) {
         "use strict";
-        var rule = this, ruleCount = 0, MAX_RULES = 499;
+        var rule = this,
+            ruleCount = 0,
+            limit = options ? options["max-rules"] : 0;
+
+        limit = limit || 0;
 
         parser.addListener("startrule", function() {
             ruleCount += 1;
         });
 
         parser.addListener("endstylesheet", function() {
-            if (ruleCount > MAX_RULES) {
+            if (limit && limit < ruleCount) {
                 reporter.report("There are too many rules (" + ruleCount + ") in the stylesheet.", 0, 0, rule);
             }
         });
